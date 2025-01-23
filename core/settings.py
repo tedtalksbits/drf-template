97606ca6a4d76943e9f25ee8377934fee2222b43
tedtalksbits <tedaneblake@gmail.com>
2025-01-22 20:31:10 -0500

    feat: update database settings to use PostgreSQL and add required dependencies

--- core/settings.py
+++ core/settings.py
@@ -158,11 +158,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
